89faeeb131b3b200ef34b1584490622f7c95c8b3
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-06-13 20:12:19 +0530

    refactor: remove accounts freezing settings from accounts settings

--- erpnext/accounts/doctype/accounts_settings/accounts_settings.py
+++ erpnext/accounts/doctype/accounts_settings/accounts_settings.py
@@ -23,7 +23,6 @@ class AccountsSettings(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		acc_frozen_upto: DF.Date | None
 		add_taxes_from_item_tax_template: DF.Check
 		add_taxes_from_taxes_and_charges_template: DF.Check
 		allow_multi_currency_invoices_against_single_party_account: DF.Check
@@ -49,7 +48,6 @@ class AccountsSettings(Document):
 		enable_immutable_ledger: DF.Check
 		enable_party_matching: DF.Check
 		exchange_gain_loss_posting_date: DF.Literal["Invoice", "Payment", "Reconciliation Date"]
-		frozen_accounts_modifier: DF.Link | None
 		general_ledger_remarks_length: DF.Int
 		ignore_account_closing_balance: DF.Check
 		ignore_is_opening_check_for_reporting: DF.Check
